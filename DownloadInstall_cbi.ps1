5dbdcfca5f0d5cc521bc3b43a4206e5e71761409
robertpriedl <robert.priedl@hotmail.com>
2021-06-06 14:24:46 +0200

    add function to download

--- DownloadInstall_cbi.ps1
+++ DownloadInstall_cbi.ps1
@@ -1,7 +1,46 @@
+function DownloadFilesFromRepo {
+Param(
+    [string]$Owner,
+    [string]$Repository,
+    [string]$Path,
+    [string]$DestinationPath
+    )
+
+    $baseUri = "https://api.github.com/"
+    $args = "repos/$Owner/$Repository/contents/$Path"
+    $wr = Invoke-WebRequest -Uri $($baseuri+$args)
+    $objects = $wr.Content | ConvertFrom-Json
+    $files = $objects | where {$_.type -eq "file"} | Select -exp download_url
+    $directories = $objects | where {$_.type -eq "dir"}
+    
+    $directories | ForEach-Object { 
+        DownloadFilesFromRepo -Owner $Owner -Repository $Repository -Path $_.path -DestinationPath $($DestinationPath+$_.name)
+    }
+
+    
+    if (-not (Test-Path $DestinationPath)) {
+        # Destination path does not exist, let's create it
+        try {
+            New-Item -Path $DestinationPath -ItemType Directory -ErrorAction Stop
+        } catch {
+            throw "Could not create path '$DestinationPath'!"
+        }
+    }
+
+    foreach ($file in $files) {
+        $fileDestination = Join-Path $DestinationPath (Split-Path $file -Leaf)
+        try {
+            Invoke-WebRequest -Uri $file -OutFile $fileDestination -ErrorAction Stop -Verbose
+            "Grabbed '$($file)' to '$fileDestination'"
+        } catch {
+            throw "Unable to download '$($file.path)'"
+        }
+    }
+
+}
+DownloadFilesFromRepo -Owner robertpriedl -Repository cloudbaseinit -DestinationPath "$env:Temp"
+
 Invoke-RestMethod -Uri "https://cloudbase.it/downloads/CloudbaseInitSetup_x64.msi" -Outfile "$env:temp\CloudbaseInitSetup_Stable_x64.msi"
-Invoke-WebRequest -Uri "https://github.com/robertpriedl/cloudbaseinit/blob/main/cloudbase-init-unattend.conf" -Outfile "$env:temp\cloudbase-init-unattend.conf"
-Invoke-WebRequest -Uri "https://github.com/robertpriedl/cloudbaseinit/blob/main/cloudbase-init.conf" -Outfile "$env:temp\cloudbase-init.conf"
-Invoke-WebRequest -Uri "https://github.com/robertpriedl/cloudbaseinit/blob/main/Unattend.xml" -Outfile "$env:temp\Unattend.xml"
 
 msiexec /i "$env:temp\CloudbaseInitSetup_Stable_x64.msi" /qn /l*v "C:\Windows\Temp\CloudInit.log" LOGGINGSERIALPORTNAME=COM1
 copy-item -path "$env:temp\cloudbase-init.conf" -destination "$env:programfiles\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf"
